06a48f6b66f2ee7bf7037ea9b440a1e9b7e2ed3f
tí khéo tay <tikheotayy@gmail.com>
2024-09-16 16:18:00 +0700

    olala

--- thực hành/src/app.js
+++ thực hành/src/app.js
@@ -5,4 +5,16 @@ const app = express();
 app.get(`/products`, (req, res) => {
     console.log("GET /products");
 });
+app.get(`/products:id`, (req, res) => {
+    console.log("GET /products");
+});
+app.get(`/products`, (req, res) => {
+    console.log("GET /products");
+});
+app.get(`/products`, (req, res) => {
+    console.log("GET /products");
+});
+app.get(`/products`, (req, res) => {
+    console.log("GET /products");
+});
 export const viteNodeApp = app;
