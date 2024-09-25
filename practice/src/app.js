6ce409721ebd08e7dcf18a1115bfc1cc3803b532
tí khéo tay <tikheotayy@gmail.com>
2024-09-26 03:04:35 +0700

    olala

--- practice/src/app.js
+++ practice/src/app.js
@@ -1,34 +1,31 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000;
+const handlebars = require('express-handlebars');
+const configViewEngine = require('./config/viewEngine');
 
-//http logger
-app.use(morgan('combined'));
+require('dotenv').config();
+const hostname = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
+//config view engine
+configViewEngine(app);
+
 
 //static file
-app.use(express.static(path.join(__dirname, 'public'))); ``
+app.use(express.static(path.join(__dirname, 'public')));
 //template engine
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'views')));
 
 //routes init
 app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/search', (req, res) => {
-    res.render('search');
-})
-app.get('/olala', (req, res) => {
-    res.render('olala');
+    res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+
+
+
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`);
 })
---
+++ practice/src/config/viewEngine.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const configViewEngine = (app) => {
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '../views'));
+    //static file
+    app.use(express.static(path.join(__dirname, 'public')));
+}
+
+module.exports = configViewEngine;
